e3d4d109925e50ca129721ec8457b7a14726c8aa
Rahul Dubey <37729273+LogiCule@users.noreply.github.com>
2020-05-05 13:21:12 +0530

    Create 01_Python_Basics.py

---
+++ Python_Track/Introduction to Python/01_Python_Basics.py
@@ -0,0 +1,31 @@
+#The Python Interface
+
+print(7+10)
+
+#Python as a calculator
+
+print(100*1.1**7)
+
+#Variable Assignment
+
+savings=100
+print(savings)
+
+#Calculations with variables
+
+growth_multiplier = 1.1
+result=savings*growth_multiplier**7
+print(result)
+
+#Other variable types
+
+desc='compound interest'
+profitable=True
+
+#operations with other types
+
+year1=growth_multiplier*savings
+print(type(year1))
+doubledesc=desc+desc
+print(doubledesc)
+
