0b59198e3c60a8f365018d68e896dba937aa4cf3
Rahul Dubey <37729273+LogiCule@users.noreply.github.com>
2020-05-06 12:02:32 +0530

    Create 03_Functions_and_Packages.py

---
+++ Python_Track/Introduction to Python/03_Functions_and_Packages.py
@@ -0,0 +1,32 @@
+#Familiar functions
+
+print(type(var1))
+print(len(var1))
+out2 = int(var2)
+
+#Multiple arguments
+
+full=first+second
+full_sorted=sorted(full,reverse=True)
+print(full_sorted)
+
+#String methods
+
+place_up=place.upper()
+print(place)
+print(place_up)
+print(place.count('o'))
+
+#List Methods
+
+print(areas.index(20.0))
+print(areas.count(9.50))
+
+#List Methods (2)
+
+areas.append(24.5)
+areas.append(15.45)
+print(areas)
+areas.reverse()
+print(areas)
+
