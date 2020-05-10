f27aef6cefca209649aeb05a8f8c1bf3dab93dc1
Rahul Dubey <37729273+LogiCule@users.noreply.github.com>
2020-05-10 21:31:37 +0530

    Update 03_Logic_Control_Flow_and_Filtering.py

--- Python_Track/Intermediate Python/03_Logic_Control_Flow_and_Filtering.py
+++ Python_Track/Intermediate Python/03_Logic_Control_Flow_and_Filtering.py
@@ -1,1 +1,31 @@
+#Equality
+
+True==False
+-5*15!=75
+'pyscript'=='PyScript'
+True==1
+
+#Greater and less than
+
+print(x>=-10)
+print('test'<=y)
+print(True>False)
+
+#Compare arrays
+
+print(my_house>=18)
+print(my_house<your_house)
+
+#and, or, not (1)
+
+print(my_kitchen>10 and my_kitchen<18)
+print(my_kitchen<14 or my_kitchen>17)
+print(2*my_kitchen<3*your_kitchen)
+
+#Boolean operators with Numpy
+
+print(np.logical_or(my_house>18.5, my_house<10))
+print(np.logical_and(my_house<11,your_house<11))
+
+#skipped the if else part
 
