fb4a5c01e702ec60b2ebca67300eb534fa9e32ce
Rahul Dubey <37729273+LogiCule@users.noreply.github.com>
2020-05-18 22:00:12 +0530

    Update 01_Introduction_and_flat_files.py

--- Python_Track/Introduction to Importing Data in Python/01_Introduction_and_flat_files.py
+++ Python_Track/Introduction to Importing Data in Python/01_Introduction_and_flat_files.py
@@ -1,1 +1,15 @@
+#Importing entire text files
 
+file = open('moby_dick.txt', 'r')
+print(file.read())
+print(file.closed)
+file.close()
+print(file.closed)
+
+#Importing text files line by line
+
+with open('moby_dick.txt') as file:
+    print(file.readline())
+    print(file.readline())
+    print(file.readline())
+    
