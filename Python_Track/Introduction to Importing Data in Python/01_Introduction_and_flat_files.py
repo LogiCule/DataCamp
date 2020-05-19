967e8399ee62f68e6941a12f29fe600de4b2436f
Rahul Dubey <37729273+LogiCule@users.noreply.github.com>
2020-05-19 10:31:41 +0530

    Update 01_Introduction_and_flat_files.py

--- Python_Track/Introduction to Importing Data in Python/01_Introduction_and_flat_files.py
+++ Python_Track/Introduction to Importing Data in Python/01_Introduction_and_flat_files.py
@@ -13,3 +13,33 @@ with open('moby_dick.txt') as file:
     print(file.readline())
     print(file.readline())
     
+#Using NumPy to import flat files
+
+import numpy as np
+file = 'digits.csv'
+digits = np.loadtxt(file, delimiter=',')
+print(type(digits))
+im = digits[21, 1:]
+im_sq = np.reshape(im, (28, 28))
+plt.imshow(im_sq, cmap='Greys', interpolation='nearest')
+plt.show()
+
+#Customizing your NumPy import
+
+import numpy as np
+file = 'digits_header.txt'
+data = np.loadtxt(file, delimiter='\t', skiprows=1, usecols=[0,2])
+print(data)
+
+#Importing different datatypes
+
+file = 'seaslug.txt'
+data = np.loadtxt(file, delimiter='\t', dtype=str)
+print(data[0])
+data_float = np.loadtxt(file, delimiter='\t', dtype=float, skiprows=1)
+print(data_float[9])
+plt.scatter(data_float[:, 0], data_float[:, 1])
+plt.xlabel('time (min.)')
+plt.ylabel('percentage of larvae')
+plt.show()
+
