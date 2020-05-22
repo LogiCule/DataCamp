3d786351efce5db4a8890f84ef0df4a44f4700bb
Rahul Dubey <37729273+LogiCule@users.noreply.github.com>
2020-05-22 10:17:40 +0530

    Update 02_Introduction_to_other_file_types.py

--- Python_Track/Introduction to Importing Data in Python/02_Introduction_to_other_file_types.py
+++ Python_Track/Introduction to Importing Data in Python/02_Introduction_to_other_file_types.py
@@ -42,4 +42,38 @@ plt.show()
 df = pd.read_stata('disarea.dta')
 print(df.head())
 
-#
+#Using h5py to import HDF5 files
+
+import numpy as np
+import h5py
+file = 'LIGO_data.hdf5'
+data = h5py.File(file, 'r')
+print(type(data))
+for key in data.keys():
+    print(key)
+    
+#Extracting data from your HDF5 file
+
+group = data['strain']
+for key in group.keys():
+    print(key)
+strain = group['Strain'].value
+num_samples = 10000
+time = np.arange(0, 1, 1/num_samples)
+plt.plot(time, strain[:num_samples])
+plt.xlabel('GPS Time (s)')
+plt.ylabel('strain')
+plt.show()
+
+#Loading .mat files
+
+import scipy.io
+mat = scipy.io.loadmat('albeck_gene_expression.mat')
+print(type(mat))
+
+#The structure of .mat in Python
+
+print(mat.keys())
+print(type(mat['CYratioCyt']))
+print(mat['CYratioCyt'].shape)
+
