ff6adcd5087317f430a92a7da29aa01f4f825fbc
Rahul Dubey <37729273+LogiCule@users.noreply.github.com>
2020-05-29 21:29:10 +0530

    Update 02_Interacting_with_APIs_to_import_data_from_the_web.py

--- Python_Track/Intermediate Importing Data in Python/02_Interacting_with_APIs_to_import_data_from_the_web.py
+++ Python_Track/Intermediate Importing Data in Python/02_Interacting_with_APIs_to_import_data_from_the_web.py
@@ -1,1 +1,22 @@
+#Loading and exploring a JSON
+
+with open("a_movie.json") as json_file:
+    json_data = json.load(json_file)
+for k in json_data.keys():
+    print(k + ': ',json_data[k])
+    
+#API requests
+
+import requests
+url = 'http://www.omdbapi.com/?apikey=72bc447a&t=the+social+network'
+r = requests.get(url)
+print(r.text)
+
+#JSON–from the web to Python
+
+url = 'http://www.omdbapi.com/?apikey=72bc447a&t=social+network'
+r = requests.get(url)
+json_data = r.json()
+
+#Checking out the Wikipedia API
 
