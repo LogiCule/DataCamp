a63d71bfde5e076221cc6871ca24aed2532a41f8
Rahul Dubey <37729273+LogiCule@users.noreply.github.com>
2020-05-30 07:02:29 +0530

    Update 02_Interacting_with_APIs_to_import_data_from_the_web.py

--- Python_Track/Intermediate Importing Data in Python/02_Interacting_with_APIs_to_import_data_from_the_web.py
+++ Python_Track/Intermediate Importing Data in Python/02_Interacting_with_APIs_to_import_data_from_the_web.py
@@ -20,3 +20,9 @@ json_data = r.json()
 
 #Checking out the Wikipedia API
 
+import requests
+url = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=pizza"
+r = requests.get(url)
+json_data = r.json()
+pizza_extract = json_data['query']['pages']['24768']['extract']
+print(pizza_extract)
