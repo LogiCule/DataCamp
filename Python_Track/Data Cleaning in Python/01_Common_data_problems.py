aba16aefa5f29815c2585f83a450da457a2d8b2d
Rahul Dubey <37729273+LogiCule@users.noreply.github.com>
2020-06-01 02:11:42 +0530

    Update 01_Common_data_problems.py

--- Python_Track/Data Cleaning in Python/01_Common_data_problems.py
+++ Python_Track/Data Cleaning in Python/01_Common_data_problems.py
@@ -1,1 +1,21 @@
+#Numeric data or ... ?
 
+print(ride_sharing.info())
+print(ride_sharing['user_type'].describe())
+
+print(ride_sharing.info())
+print(ride_sharing['user_type'].describe())
+ride_sharing['user_type_cat'] = ride_sharing['user_type'].astype('category')
+assert ride_sharing['user_type_cat'].dtype == 'category'
+print(ride_sharing['user_type_cat'].describe())
+
+#Summing strings and concatenating numbers
+
+ride_sharing['duration_trim'] = ride_sharing['duration'].str.strip("minutes")
+ride_sharing['duration_time'] = ride_sharing['duration_trim'].astype('int')
+assert ride_sharing['duration_time'].dtype == 'int'
+print(ride_sharing[['duration','duration_trim','duration_time']])
+import numpy as np
+print(np.mean(ride_sharing['duration_time']))
+
+#
